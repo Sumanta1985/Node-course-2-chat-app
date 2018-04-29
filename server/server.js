dabc695e9279520aad4532f30ca7fffc5f68a856
Alolika  Dutta <alolikadutta@Alolikas-MacBook-Air.local>
2018-04-28 20:26:29 -0400

    1st class socket.io

--- server/server.js
+++ server/server.js
@@ -1,19 +1,32 @@
 const path=require('path');
+const http=require('http');
+const socketIO=require('socket.io');
 const express=require('express');
 
 const port=process.env.port||3000;
 
 const app=express();
 
+const server=http.createServer(app);
+
 const publicPath=path.join(__dirname,'../public');
+
+var io=socketIO(server);  //web socket server
 
 app.use(express.static(publicPath));
 
-app.get('/',(doc)=>{
-  cnsole.log('document',doc);
+io.on('connection',(socket)=>{
+  console.log("new user connected");
+  socket.on('disconnect',()=>{
+    console.log('Client connection closed');
+  });
+});
+
+app.get('/',(req,res)=>{
+  console.log('document',req);
 });
 
 
-app.listen(port,()=>{
+server.listen(port,()=>{
   console.log(`Server started at port ${port}`);
 });
