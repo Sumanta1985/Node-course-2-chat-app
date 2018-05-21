597b09836f5a12d3d2e2e9f187256d175c2fad0e
Alolika  Dutta <alolikadutta@Alolikas-MacBook-Air.local>
2018-05-20 21:36:14 -0400

    comment updated in chat.js

--- server/utils/users.js
+++ server/utils/users.js
@@ -5,6 +5,9 @@
 
 class Users{
   constructor(){
+    // below 2 lines are notes
+    //var user=new Users();
+    //user.users will return an array and it is going to be an array of obects because of adduser funtion.
     this.users=[];
   }
   adduser(id,name,c_name){
